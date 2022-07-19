7b6570067026c78a3da426ac9df849e4d76cad92
joelma <joelmadev@gmail.com>
2022-07-19 10:08:00 -0300

    Criando uma lista de notas e o metodo GET

---
+++ src/main/kotlin/com/jibs/ceepws/controller/NoteController.kt
@@ -0,0 +1,20 @@
+package com.jibs.ceepws.controller
+
+import com.jibs.ceepws.model.NoteModel
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller //agora sera capaz de atender requisicoes
+@RequestMapping("notes")//o endereco que ira atender
+class NoteController {
+
+    @GetMapping
+    @ResponseBody//sem isso leva um erro 500 por que o controller nao sabe lidar com esse dtipo de retorno
+    fun list(): List<NoteModel> {
+        return listOf(NoteModel("leitura", "livro de spring boot"),
+            NoteModel("pesquisa", "ambiente com Docker"))
+
+    }
+}
---
+++ src/main/kotlin/com/jibs/ceepws/model/NoteModel.kt
@@ -0,0 +1,6 @@
+package com.jibs.ceepws.model
+
+class NoteModel(
+    val title: String,
+    val description: String,
+)
