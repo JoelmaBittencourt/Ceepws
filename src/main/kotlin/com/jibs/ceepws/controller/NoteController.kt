ce97e9f4f2fe68237eafd54ff5c7e1c410103487
joelma <joelmadev@gmail.com>
2022-07-19 10:16:57 -0300

    Retirando @Requestbody e adicionado @RestController

--- src/main/kotlin/com/jibs/ceepws/controller/NoteController.kt
+++ src/main/kotlin/com/jibs/ceepws/controller/NoteController.kt
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller //agora sera capaz de atender requisicoes
+@RestController //agora sera capaz de atender requisicoes
 @RequestMapping("notes")//o endereco que ira atender
 class NoteController {
 
     @GetMapping
-    @ResponseBody//sem isso leva um erro 500 por que o controller nao sabe lidar com esse dtipo de retorno
+    //@ResponseBody//sem isso leva um erro 500 por que o controller nao sabe lidar com esse dtipo de retorno
     fun list(): List<NoteModel> {
         return listOf(NoteModel("leitura", "livro de spring boot"),
             NoteModel("pesquisa", "ambiente com Docker"))
